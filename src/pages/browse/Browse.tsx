d72e906b0b0193980fc2b1c2ce4105153453a1ad
MohamedJamaal <mohamed_gamal19@hotmail.com>
2020-12-30 23:04:09 +0200

    Add movie description

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -5,7 +5,11 @@ export default function Browse() {
     return (
         <div className="browse-container">
             <div className="thumbnail-section">
-                <div className="thumbnail" style={{ backgroundImage: `url(assets/images/popcorn.jpg)` }}></div>
+                <div className="thumbnail"
+                    style={{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5) 15%, transparent 60%), url(assets/images/popcorn.jpg)` }}></div>
+                <span className="description"> This is the description of popcorn movie,
+                    we hope you will enjoy the movie
+                </span>
             </div>
         </div>
     );
