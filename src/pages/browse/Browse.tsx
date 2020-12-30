048926477aab249ce357fc4fd960e0776af2955d
MohamedJamaal <mohamed_gamal19@hotmail.com>
2020-12-30 04:43:45 +0200

    Add thumbnail photo and ingredient to navbar

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -2,5 +2,11 @@ import React from 'react'
 import './Browse.css'
 
 export default function Browse() {
-    return <div>Browse</div>
+    return (
+        <div className="browse-container">
+            <div className="thumbnail-section">
+                <div className="thumbnail" style={{ backgroundImage: `url(assets/images/popcorn.jpg)` }}></div>
+            </div>
+        </div>
+    );
 }
