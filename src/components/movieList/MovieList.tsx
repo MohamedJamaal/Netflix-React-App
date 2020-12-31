6ee937aa40ce1c4eda8a3c22eb46c974a41cd6ed
MohamedJamaal <mohamed_gamal19@hotmail.com>
2020-12-31 19:57:48 +0200

    Add next&previous  buttons and its styles

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './MovieList.css'
 import movies from "../../data/movies/movies.json"
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+
 
 export default function MovieList() {
     return (
@@ -8,14 +11,18 @@ export default function MovieList() {
             <span className="title">Trending Now</span>
             <div className="movies" >
                 <section id="section-1">
+                    <a className="previous" href="#section-2"><ChevronLeftIcon /></a>
                     {movies.slice(0, 5).map((movie, index) => (
                         <div className="movie" style={{backgroundImage: `url(${movie.image_src})`}}></div>
                     ))}
+                    <a className="next" href="#section-2"><ChevronRightIcon /></a>
                 </section>
                 <section id="section-2">
+                <a className="previous" href="#section-1"><ChevronLeftIcon /></a>
                     {movies.slice(5, movies.length).map((movie, index) => (
                         <div className="movie" style={{backgroundImage: `url(${movie.image_src})`}}></div>
                     ))}
+                    <a className="next" href="#section-1"><ChevronRightIcon /></a>
                 </section>
             </div>
         </div>
